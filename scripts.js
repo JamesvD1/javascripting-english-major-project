376d2d22d8cb0902dcb5ed98dc3fa43ba7915127
JamesvD1 <99975306+JamesvD1@users.noreply.github.com>
2022-02-18 10:05:34 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,2 +1,9 @@
 alert("scripts.js has loaded!");
 $("#response").html("scripts.js has loaded!");
+let userInput;
+userInput = "samosa";
+if ( userInput === "burrito" ) {
+  $("#response").html("Brilliant choice!");
+} else {
+  $("#response").html("Don’t you want a burrito?");
+}

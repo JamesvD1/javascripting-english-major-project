e29bdefc8bbc95d1eb907803797e0ac1032ab4ad
JamesvD1 <99975306+JamesvD1@users.noreply.github.com>
2022-02-25 09:31:10 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,17 @@
-let userInput;
+/*let userInput;
 userInput = prompt("What do you want to have for dinner?", "Type your answer here.");
 if ( userInput === "burrito" ) {
   $("#response").html("Brilliant choice!");
 } else {
   $("#response").html("Don’t you want a burrito?");
 }
+*/
+
+let userInput;
+userInput = prompt("do you want to sleep?", "Type your answer here.");
+if ( userInput === "yes" ) [
+  $("#response").html("me too!");
+  } else {
+    $("#response").html("you need it, ugly.");
+  }
+

e01fc0dbf07d5167688e8a330b8de6cbf611868e
JamesvD1 <99975306+JamesvD1@users.noreply.github.com>
2022-03-04 09:49:43 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -30,7 +30,40 @@ blackBeans = "black";
 makeABurrito(blackBeans); 
 */
 
-let arrayOfStrings, arrayOfNumbers, arrayMixed;
+/*let arrayOfStrings, arrayOfNumbers, arrayMixed;
 arrayOfStrings = ["a", "b", "c"];
 arrayOfNumbers = [1, 2, 3];
 arrayMixed = ["a", 1, null, true, arrayOfNumbers, [4.5, 5.6]];
+$("#response").html(arrayOfStrings[2]);
+*/
+
+/*let myBurritoObject;
+myBurritoObject = {
+  tortilla :"wheat" , 
+  guacamole : true, 
+  beans: "pinto" , 
+  habaneroSauceSquirts: 3 
+};
+$("#response").html(myBurritoObject["tortilla"]);
+*/
+
+let myHabaneroSauceSquirts, myBurritoObject;
+myHabaneroSauceSquirts = 3;
+myBurritoObject = {
+  tortilla: "wheat" ,
+  guacaole: true 
+  beans: "pinto", 
+habaneroSaucweSquirts : myHabaneroSauceSquirts, 
+spiciness function(){
+  if (myHabaneroSauceSquirts > 0 ){
+    alert("this is a spicy burrito!");
+  } else {
+    alert("this is a mild burrito.");
+  }
+}
+};
+$("#response").html("Your burrito has " +
+  myBurritoObject.habaneroSauceSquirts +
+  " squirts of habanero.");
+myBurritoObject.spiciness();
+

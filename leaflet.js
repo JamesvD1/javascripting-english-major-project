581ffe9f33b5b4f69700eba412e9394b6bd4bab5
JamesvD1 <99975306+JamesvD1@users.noreply.github.com>
2022-03-25 09:56:42 -0400

    Update leaflet.js

--- leaflet.js
+++ leaflet.js
@@ -1,5 +1,3 @@
-let firstMap;
-firstMap = L.map("first-map");
 let firstMap, tileLayer;
 firstMap = L.map("first-map");
 tileLayer =
@@ -10,4 +8,3 @@ L.tileLayer("https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x
     });
 tileLayer.addTo(firstMap);
 firstMap.setView([40.730833, -73.9975], 16);
-
